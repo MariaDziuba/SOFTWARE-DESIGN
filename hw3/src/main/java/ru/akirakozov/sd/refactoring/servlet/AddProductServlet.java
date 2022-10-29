a6aa5fcb2d1451047acb2cb2dd9809e2300d03b6
Maria <dziuba.m.o@yandex.ru>
2022-10-29 22:28:06 +0500

    [hw3] change servlets to use HTMLProductPrinter, ProductDao and ProductServlet abstract class

--- hw3/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ hw3/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -1,5 +1,8 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import ru.akirakozov.sd.refactoring.dao.ProductDAO;
+import ru.akirakozov.sd.refactoring.models.Product;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,27 +14,14 @@ import java.sql.Statement;
 /**
  * @author akirakozov
  */
-public class AddProductServlet extends HttpServlet {
+public class AddProductServlet extends ProductServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public AddProductServlet(ProductDAO productDAO) {
+        super(productDAO);
+    }
+    protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String name = request.getParameter("name");
         long price = Long.parseLong(request.getParameter("price"));
-
-        try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                String sql = "INSERT INTO PRODUCT " +
-                        "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-                Statement stmt = c.createStatement();
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        response.setContentType("text/html");
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("OK");
+        productDAO.insert(new Product(name, price));
     }
 }
